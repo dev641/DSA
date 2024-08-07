6199363a86f68dfc92049c45a3f8ec978d52705e
Devashish <dev641science@gmail.com>
2024-08-07 22:51:35 +0530

    Added new method for reversing array

--- Recursion/ReverseArray/ReverseArray.java
+++ Recursion/ReverseArray/ReverseArray.java
@@ -8,9 +8,18 @@ public class ReverseArray {
         arr[r] = temp;
         reverseArr(arr, l+1, r-1);
     }
+
+    private static void reverseArr(int[] arr, int i) {
+        if (2*i >= arr.length - 1) return;
+        int temp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = temp;
+        reverseArr(arr, i+1);
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
-        reverseArr(arr, 0, arr.length -1);
+//        reverseArr(arr, 0, arr.length -1);
+        reverseArr(arr, 0);
         for(int el: arr){
             System.out.print(el + " ");
         }
