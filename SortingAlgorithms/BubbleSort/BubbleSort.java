edec0aec782e0ee8183b5b9cf8663eaa934bba64
Devashish <dev641science@gmail.com>
2024-08-17 07:02:56 +0530

    Recursive Buuble Sort Added

--- SortingAlgorithms/BubbleSort/BubbleSort.java
+++ SortingAlgorithms/BubbleSort/BubbleSort.java
@@ -13,9 +13,21 @@ public class BubbleSort {
             }
         }
     }
+
+    public static void sort(int[] arr, int n) {
+        if (n == 0) return;
+        for (int i = 0;i<n;i++) {
+            if (arr[i]> arr[i+1]) {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        sort(arr, n-1);
+    }
     public static void main(String[] args) {
         int[] arr = {5,6,7,4,6,5,3,2};
-        sort(arr);
+        sort(arr, arr.length-1);
         for(int el: arr) {
             System.out.print(el + " ");
         }
