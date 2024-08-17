73bb378aa1f8185c9961f8e54a42227ee33aa5d9
Devashish <dev641science@gmail.com>
2024-08-17 07:33:56 +0530

    Added recursive insertion sort

--- SortingAlgorithms/InsertionSort/InsertionSort.java
+++ SortingAlgorithms/InsertionSort/InsertionSort.java
@@ -13,9 +13,23 @@ public class InsertionSort {
             }
         }
     }
+
+    public static void sort(int[] arr, int n) {
+        if (n == arr.length) return;
+        int i = n;
+        while(i > 0 && arr[i]<= arr[i-1]) {
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+            i--;
+        }
+
+        sort(arr, n+1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {5,6,7,4,6,5,3,2};
-        sort(arr);
+        sort(arr, 0);
         for(int el: arr) {
             System.out.print(el + " ");
         }
